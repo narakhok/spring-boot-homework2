7a05de85541dd7ec88edd8971e0e201082e6a379
unknown <narak.hok@Recervesmb.sambatfinance.com>
2017-11-13 09:49:48 +0700

    Spring boot homework 2 by Narak who study at kshrd

---
+++ src/main/java/com/kshrd/SpringBootHomework2Application.java
@@ -0,0 +1,12 @@
+package com.kshrd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHomework2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHomework2Application.class, args);
+	}
+}
